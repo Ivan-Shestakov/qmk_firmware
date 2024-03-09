37dfd5f06f41e55bebcc263d565f2f756e6eaf30
Ivan Shestakov <ivan.v.shestakov@gmail.com>
2024-03-09 23:39:12 +0200

    Remap pin after repair of board

--- keyboards/handwired/dactyl_manuform/5x6/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/config.h
@@ -31,7 +31,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // wiring of each half
 #define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
-#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
+//#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS { F6, F7, B1, F5, B2, B6 }  // Replace pin B3 with F5 (from 14 to A2) due to broken pad on pro-micro
 
 #define DIODE_DIRECTION COL2ROW
 
